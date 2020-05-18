22b211b58ae78f6ffe034d85e150d1fdb25890ce
Vadim <carakazov@yandex.ru>
2020-05-18 18:57:30 +0300

    My new Lab

---
+++ source/repos/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello GIT!");
+            Console.ReadKey();
+        }
+    }
+}
